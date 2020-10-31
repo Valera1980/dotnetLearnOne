17bb5ef1ff731126f2ac871a4e017276304aaa7e
Valera <gavrilow777@gmail.com>
2020-10-31 12:13:18 +0200

    add alternative delegate

--- project/src/delegates/delegate.one.cs
+++ project/src/delegates/delegate.one.cs
@@ -4,14 +4,16 @@ namespace DelegatesExamples
     {
         private delegate string AppendParam(string _append);
 
-        private string _append(string p)
-        {
-            return p + " 10.10.2020";
-        }
+       
         public string getMessage(string p)
         {
-            var append = new AppendParam(_append);
-            return append(p);
+            // var append = new AppendParam(_append); one method
+            AppendParam del = _append;         // two method
+            return del(p);
+        }
+         private string _append(string p)
+        {
+            return p + " 10.10.2020";
         }
     }
 }
